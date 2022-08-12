9ce074592e0de906f3c7e0dfa16af3ff201a7c31
CristianRamirezC <cristiancrc97@hotmail.com>
2022-08-12 16:16:18 -0500

    adjustments to character item view layout applied

--- app/src/main/java/com/example/rickandmortyapp/model/CharacterProvider.kt
+++ app/src/main/java/com/example/rickandmortyapp/model/CharacterProvider.kt
@@ -5,75 +5,75 @@ class CharacterProvider {
         val characterList = listOf<Character>(
             Character(
                 characterID = 821,
-                characterName = "Gotron",
+                characterName = "Rick Sanchez",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/821.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            ),
+            Character(
+                characterID = 821,
+                characterName = "Morty Smith",
+                characterStatus = "unknown",
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            ),
+            Character(
+                characterID = 821,
+                characterName = "Summer Smith",
+                characterStatus = "unknown",
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+            ),
+            Character(
+                characterID = 821,
+                characterName = "Beth Smith",
+                characterStatus = "unknown",
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+            ),
+            Character(
+                characterID = 821,
+                characterName = "Jerry Smith",
+                characterStatus = "unknown",
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/5.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/822.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/6.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/7.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/824.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/8.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/825.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/9.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/826.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/10.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/816.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/11.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/817.jpeg"
-            ),
-            Character(
-                characterID = 821,
-                characterName = "Gotron",
-                characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/818.jpeg"
-            ),
-            Character(
-                characterID = 821,
-                characterName = "Gotron",
-                characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
-            ),
-            Character(
-                characterID = 821,
-                characterName = "Gotron",
-                characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
-            ),
-            Character(
-                characterID = 821,
-                characterName = "Gotron",
-                characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/12.jpeg"
             )
         )
     }
