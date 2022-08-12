e9e1064fdb5986803e1ffb8f32789d34140e7ade
CristianRamirezC <cristiancrc97@hotmail.com>
2022-08-12 15:58:23 -0500

    Glide dependency added for setting images on ImageViews, recycler view item images set up

--- app/src/main/java/com/example/rickandmortyapp/UI/character/adapter/CharacterViewHolder.kt
+++ app/src/main/java/com/example/rickandmortyapp/UI/character/adapter/CharacterViewHolder.kt
@@ -2,15 +2,18 @@ package com.example.rickandmortyapp.UI.character.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.rickandmortyapp.R
 import com.example.rickandmortyapp.databinding.CharacterViewBinding
 import com.example.rickandmortyapp.model.Character
 
-class CharacterViewHolder(private var view: View): RecyclerView.ViewHolder(view) {
+class CharacterViewHolder(private var view: View) : RecyclerView.ViewHolder(view) {
 
     private val binding = CharacterViewBinding.bind(view)
     fun render(character: Character) {
-        binding.ivCharacterImage
         binding.tvCharacterName.text = character.characterName
+        Glide.with(view.context)
+            .load(character.characterImage)
+            .into(binding.ivCharacterImage)
     }
 }
--- app/src/main/java/com/example/rickandmortyapp/model/CharacterProvider.kt
+++ app/src/main/java/com/example/rickandmortyapp/model/CharacterProvider.kt
@@ -25,37 +25,37 @@ class CharacterProvider {
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/824.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/825.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/826.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/816.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/817.jpeg"
             ),
             Character(
                 characterID = 821,
                 characterName = "Gotron",
                 characterStatus = "unknown",
-                characterImage = "https://rickandmortyapi.com/api/character/avatar/823.jpeg"
+                characterImage = "https://rickandmortyapi.com/api/character/avatar/818.jpeg"
             ),
             Character(
                 characterID = 821,
