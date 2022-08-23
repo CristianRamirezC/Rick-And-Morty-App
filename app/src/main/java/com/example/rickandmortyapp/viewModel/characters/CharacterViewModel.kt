c409dcc02d49eb1001a9adb33b579f1c334da807
CristianRamirezC <cristiancrc97@hotmail.com>
2022-08-23 10:01:37 -0500

    creating viewModels

---
+++ app/src/main/java/com/example/rickandmortyapp/viewModel/characters/CharacterViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.rickandmortyapp.viewModel.characters
+
+class CharacterViewModel {
+}
---
+++ app/src/main/java/com/example/rickandmortyapp/viewModel/episodes/EpisodeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.rickandmortyapp.viewModel.episodes
+
+class EpisodeViewModel {
+}
