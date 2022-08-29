11514dcb747500e203effb43ee86e9b99d7a5b56
CristianRamirezC <cristiancrc97@hotmail.com>
2022-08-29 18:24:57 -0500

    api url strings changed to const vals

--- app/src/main/java/com/example/rickandmortyapp/data/characters/APICharactersService.kt
+++ app/src/main/java/com/example/rickandmortyapp/data/characters/APICharactersService.kt
@@ -4,9 +4,11 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
+private const val GET_CHARACTER_ENDPOINT_URL: String = "character?page=1"
+
 interface APICharactersService {
     @GET
     suspend fun getCharacters(
-        @Url url: String = "character?page=1"
+        @Url url: String = GET_CHARACTER_ENDPOINT_URL
     ): Response<CharactersResponse>
 }
--- app/src/main/java/com/example/rickandmortyapp/data/episodes/APIEpisodesService.kt
+++ app/src/main/java/com/example/rickandmortyapp/data/episodes/APIEpisodesService.kt
@@ -4,9 +4,11 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
+private const val GET_EPISODE_ENDPOINT_URL: String = "episode#"
+
 interface APIEpisodesService {
     @GET
     suspend fun getEpisodes(
-        @Url url: String = "episode#"
+        @Url url: String = GET_EPISODE_ENDPOINT_URL
     ): Response<EpisodesResponse>
 }
